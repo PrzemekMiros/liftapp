e3a5b1dd9bf3b9e89e45e433cdd0cabaa8d6a69e
PrzemekMiros <kontakt@futurewebstudio.pl>
2026-01-07 17:51:19 +0100

    Add root index screen to stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ export default function RootLayout() {
   return (
     <WorkoutProvider>
       <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="workout/active" options={{ presentation: 'modal', title: 'Aktywny Trening' }} />
       </Stack>
